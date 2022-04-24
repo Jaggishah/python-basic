b2c6d8fada0cefb190932ac98fc1ba8d693cb477
Jugdeep Singh <83028805+Jaggishah@users.noreply.github.com>
2022-04-24 14:46:43 -0700

    Add files via upload

---
+++ exception.py
@@ -0,0 +1,10 @@
+# program to print the reciprocal of even numbers
+
+try:
+    a = input('enter a value=')
+    if(a % 2 == 0):
+        print('value is odd')
+except:
+    print('value is ')
+finally:
+    print('you run a odd even code')
